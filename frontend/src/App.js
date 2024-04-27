159e6b2efc1df7efd6abf3cd70228122ed20dd28
bolshialex <bolshialex000@gmail.com>
2024-04-26 23:11:48 -0700

    Working on Login and signup page
    Learning and implementing bootstrap on the login page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
