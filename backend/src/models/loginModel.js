495594badab9517a3631b5c3e786f19c22f38335
bolshialex <bolshialex000@gmail.com>
2024-05-11 17:32:57 -0700

    Save the react files for login Delete other files from front end

--- backend/src/models/loginModel.js
+++ backend/src/models/loginModel.js
@@ -5,9 +5,28 @@ const loginSchema = {
     connectDb.query(`SELECT FROM employee WHERE email = '${email}'`, callback);
   },
   findEmployee: (email, callback) => {
+    if (!validateEmail(email)) {
+      callback(new Error("Invalid email format"));
+      return;
+    }
+
     connectDb.query(
-      `SELECT * FROM employee WHERE email LIKE '${email}'`,
-      callback
+      "SELECT * FROM employee WHERE email = ?",
+      [email],
+      (error, results) => {
+        if (error) {
+          callback(error);
+          return;
+        }
+
+        if (results.length === 0) {
+          callback(new Error("Employee not found"));
+          return;
+        }
+
+        // Successful query
+        callback(null, results[0]);
+      }
     );
   },
 };
--- saves/App.js
+++ saves/App.js
@@ -3,13 +3,15 @@ import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
